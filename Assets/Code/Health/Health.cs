6de91188f6886102a9b5721581e97794cc8939cf
thuong <buiminhthuong30122005@gmail.com>
2026-01-02 12:55:58 +0700

    add health and healthBar

---
+++ Assets/Code/Health/Health.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    private float startHealth;
+    public float currentHealth {get; private set;}
+    private Animator animator;
+    private bool dead;
+    
+    private void Awake()
+    {
+        currentHealth = startHealth;
+        animator = GetComponent<Animator>();
+    }
+
+    public void takeDamage(float damage)
+    {
+        currentHealth = Mathf.Clamp(currentHealth - damage, 0, startHealth);
+
+        if (currentHealth > 0)
+        {
+            animator.SetTrigger("Hurt");
+        }
+        else
+        {
+            if (!dead)
+            {
+                animator.SetTrigger("Die");
+                GetComponent<moverment>().enabled = false;
+                dead = true;
+            }
+        }
+    }
+}
---
+++ Assets/Code/Health/HealthBar.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Net.Mime;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class H : MonoBehaviour
+{
+    [SerializeField] private Health playerHealth;
+    [SerializeField] private Image totalHealthBar;
+    [SerializeField] private Image currentHealthBar;
+
+    private void Start()
+    {
+        totalHealthBar.fillAmount = playerHealth.currentHealth / 10;
+    }
+
+    private void Update()
+    {
+        currentHealthBar.fillAmount = playerHealth.currentHealth / 10;
+    }
+}
