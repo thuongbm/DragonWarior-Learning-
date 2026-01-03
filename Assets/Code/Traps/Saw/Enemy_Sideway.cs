8328d2f05620ae16d43151931ff4e4d109f14cc6
thuong <buiminhthuong30122005@gmail.com>
2026-01-03 22:46:19 +0700

    add moverment speed

--- Assets/Code/Traps/Saw/Enemy_Sideway.cs
+++ Assets/Code/Traps/Saw/Enemy_Sideway.cs
@@ -5,7 +5,45 @@ using UnityEngine;
 
 public class Enemy_Sideway : MonoBehaviour
 {
-   [SerializeField] float damage;
+   [SerializeField] private float speed;
+   [SerializeField] private float damage;
+   [SerializeField] private float movermentDistance;
+   
+   private bool moveLeft;
+   private float leftEgde;
+   private float rightEgde;
+
+   private void Awake()
+   {
+      leftEgde = transform.position.x - movermentDistance;
+      rightEgde = transform.position.x + movermentDistance;
+   }
+
+   private void Update()
+   {
+      if (moveLeft)
+      {
+         if (transform.position.x > leftEgde)
+         {
+            transform.position = new Vector3(transform.position.x - speed * Time.deltaTime, transform.position.y, transform.position.z);
+         }
+         else
+         {
+            moveLeft = false;
+         }
+      }
+      else
+      {
+         if (transform.position.x < rightEgde)
+         {
+            transform.position = new Vector3(transform.position.x + speed * Time.deltaTime, transform.position.y, transform.position.z);
+         }
+         else
+         {
+            moveLeft = true;  
+         }
+      }
+   }
 
    private void OnTriggerEnter2D(Collider2D other)
    {
