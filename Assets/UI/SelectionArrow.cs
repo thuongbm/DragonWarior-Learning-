ad53b02526a924abb93f65bbb25cb0e66b35ff63
Bùi Minh Thương <148768906+thuongbm@users.noreply.github.com>
2026-02-24 12:14:00 +0700

    add option screen

--- Assets/UI/SelectionArrow.cs
+++ Assets/UI/SelectionArrow.cs
@@ -1,8 +1,52 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class SelectionArrow : MonoBehaviour
 {
-    
+    [SerializeField] private AudioClip changeSound;
+    [SerializeField] private AudioClip interactSound;
+    [SerializeField] private RectTransform[] buttons;
+    private RectTransform rect;
+    private int currentPosition;
+
+    private void Awake()
+    {
+        rect = GetComponent<RectTransform>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W))
+        {
+            ChangePosition(-1);
+        }
+
+        if (Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.S))
+        {
+            ChangePosition(1);
+        }
+    }
+
+    private void ChangePosition(int position)
+    {
+        currentPosition += position;
+
+        if (position != 0)
+        {
+            SoundManager.instance.PlaySound(changeSound);
+        }
+
+        if (currentPosition < 0)
+        {
+            currentPosition = buttons.Length - 1;
+        }
+        else if (currentPosition > buttons.Length - 1)
+        {
+            currentPosition = 0;
+        }
+        
+        rect.position = new Vector3(rect.position.x, buttons[currentPosition].position.y, 0);
+    }
 }
