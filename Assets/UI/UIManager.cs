f0f5a402f997bef1dd9df00efc5c3235795a7d7f
Bùi Minh Thương <148768906+thuongbm@users.noreply.github.com>
2026-02-26 19:20:43 +0700

    add pause game UI

--- Assets/UI/UIManager.cs
+++ Assets/UI/UIManager.cs
@@ -1,18 +1,26 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class UIManager : MonoBehaviour
 {
-    [SerializeField] GameObject gameOverScreen;
-    [SerializeField] AudioClip gameOverSound;
+    [Header("Game Over")]
+    [SerializeField] private GameObject gameOverScreen;
+    [SerializeField] private AudioClip gameOverSound;
 
+    [Header("Pause Game")]
+    [SerializeField] private GameObject pauseGameScreen;
+    
     private void Awake()
     {
         gameOverScreen.SetActive(false);
+        pauseGameScreen.SetActive(false);
     }
+
+    #region Game Over
 
     public void GameOver()
     {
@@ -33,6 +41,47 @@ public class UIManager : MonoBehaviour
     public void QuitGame()
     {
         Application.Quit();
+        
+        #if UNITY_EDITOR
         UnityEditor.EditorApplication.isPlaying = false;
+        #endif
     }
+
+    #endregion
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (pauseGameScreen.activeInHierarchy)
+            {
+                PauseGame(false);
+            }
+            else
+            {
+                PauseGame(true);
+            }
+        }
+        
+    }
+
+    #region Pause Game
+    
+    private bool paused = false;
+
+    private void PauseGame(bool status)
+    {
+        pauseGameScreen.SetActive(status);
+
+        if (status)
+        {
+            Time.timeScale = 0;
+        }
+        else
+        {
+            Time.timeScale = 1;
+        }
+    }
+    
+    #endregion
 }
